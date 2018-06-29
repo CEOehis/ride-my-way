dc6c9c93e7fa4f84c3f93dca0dee65764de1b07e
ceoehis <ceo.ehis@outlook.com>
2018-06-29 04:29:49 +0100

    feature(tests): update test database name
    - psql scripts failed due to syntatic error with previous test db name
    - changed db name from 'ridemyway-test' to 'ridemyway_test'

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ exports.setup = {
   test: {
     user: 'postgres',
     host: '127.0.0.1',
-    database: 'ridemyway-test',
+    database: 'ridemyway_test',
     password: 'postgres',
     port: 5432,
   },
