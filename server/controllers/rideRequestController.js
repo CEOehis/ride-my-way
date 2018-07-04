404700debfe7586b2aff920384197bdb865d84c5
ceoehis <ceo.ehis@outlook.com>
2018-07-04 23:04:17 +0100

    feature(requests): check if request has been responded to
    - check if status was provided in request body
    - compare created_at and updated_at timestamps to check if request
    has been responded to before

--- server/controllers/rideRequestController.js
+++ server/controllers/rideRequestController.js
@@ -100,8 +100,16 @@ export default class RideRequest {
     const { body, params, userId } = req;
     const { status } = body;
     const { requestId, rideId } = params;
+    // status can't be allowed to be undefined, otherwise pg will pass that value
+    // to the sql query where it is required without an error
+    if (typeof status === 'undefined') {
+      return res.status(400).json({
+        status: 'error',
+        message: 'Ride request status must be supplied in request body',
+      });
+    }
     // check if the user is the one who created the ride offer
-    pool
+    return pool
       .query('SELECT * FROM rides WHERE id = $1', [rideId])
       .then((result) => {
         if (result.rowCount !== 0) {
@@ -113,24 +121,40 @@ export default class RideRequest {
               message: 'You are not allowed to respond to another users ride requests',
             });
           }
+          // the created_at and updated_at columns are set by default on creation of request
+          // compare both fields to check if the request has been responded to
+          // because, ideally, user is not allowed to modify an already responded to request
           return pool
-            .query('UPDATE requests SET offerstatus = $1, updated_at = NOW() WHERE id = $2', [status, requestId])
-            .then((updateResult) => {
-              if (updateResult.rowCount) {
-                return res.status(200).json({
-                  status: 'success',
-                  message: `Successfully ${status} ride request`,
+            .query('SELECT created_at, updated_at FROM requests WHERE id = $1 and created_at = updated_at;', [requestId])
+            .then((selectResult) => {
+              if (selectResult.rowCount === 0) {
+                // the query successfully compared both columns
+                // which happen to be equal
+                return res.status(400).json({
+                  status: 'error',
+                  message: 'This request does not exist or has already been responded to',
                 });
               }
-              return res.status(500).json({
-                status: 'error',
-                message: 'Unable to respond to ride request',
-              });
+              // otherwise it is safe to update
+              return pool
+                .query('UPDATE requests SET offerstatus = $1, updated_at = NOW() WHERE id = $2', [status, requestId])
+                .then(() => {
+                  return res.status(200).json({
+                    status: 'success',
+                    message: `Successfully ${status} ride request`,
+                  });
+                })
+                .catch(() => {
+                  return res.status(500).json({
+                    status: 'error',
+                    message: 'Unable to respond to ride request, make sure status value is either accepted or rejected',
+                  });
+                });
             })
             .catch(() => {
               return res.status(500).json({
                 status: 'error',
-                message: 'Unable to respond to ride request',
+                message: 'Error fetching ride offer request',
               });
             });
         }
